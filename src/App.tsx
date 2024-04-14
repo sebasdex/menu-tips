bdf8daac0de6494fc598a51a110028f357218ac2
dexterdc <sebasdc9@hotmail.com>
2024-04-13 22:53:57 -0600

    add menu button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import { useState } from "react";
 import { menuItems } from "./libs/db.ts";
 import MenuItems from "./components/MenuItems.tsx";
+import MenuIcon from "./components/icons/MenuIcon.tsx";
+import CloseIcon from "./components/icons/CloseIcon.tsx";
 function App() {
   const [menu] = useState(menuItems);
+  const [menuOn, setMenuOn] = useState(false);
+
   return (
     <>
-      <h1 className="p-2  bg-orange-500 text-white uppercase font-bold text-center text-xl">
-        Menú de Comida
-      </h1>
+      <header className="p-2  bg-orange-500 text-white flex justify-between items-center">
+        <h1 className="uppercase font-bold text-center text-xl">
+          Menú de Comida
+        </h1>
+        <button onClick={() => setMenuOn(!menuOn)} className="md:hidden">
+          <MenuIcon />
+        </button>
+      </header>
       <main className="p-4 min-h-screen flex flex-col bg-gray-100 md:flex-row md:items-start md:gap-4">
         <MenuItems menu={menu} />
-        <section className="min-h-screen border border-orange-500 md:w-1/3"></section>
+        <section
+          className={`min-h-screen border bg-orange-500 fixed inset-0 md:w-1/3 md:block md:static
+         ${menuOn ? "block" : "hidden"}`}
+        >
+          <header className="flex justify-between text-white p-2">
+            <h2 className="p-2 bg-orange-500 text-white uppercase font-bold text-center text-xl">
+              Ordenes Recientes
+            </h2>
+            <button onClick={() => setMenuOn(!menuOn)} className="md:hidden">
+              <CloseIcon />
+            </button>
+          </header>
+        </section>
       </main>
     </>
   );
