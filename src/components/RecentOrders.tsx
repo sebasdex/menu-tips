fdc3ec2f1eb3adb86a22eb5e94696a15dd424148
DexterDC <sebasdc9@hotmail.com>
2024-04-15 13:13:35 -0600

    add total order

--- src/components/RecentOrders.tsx
+++ src/components/RecentOrders.tsx
@@ -9,7 +9,12 @@ interface RecentOrdersMenu {
 interface FoodQuantity extends Food {
   quantity: number;
 }
+
 function RecentOrders({ menuOn, setMenuOn, orders }: RecentOrdersMenu) {
+  const totalCost = orders
+    .reduce((acc, order) => acc + order.cost * order.quantity, 0)
+    .toFixed(2);
+  const totalItems = orders.reduce((acc, order) => acc + order.quantity, 0);
   return (
     <section
       className={`min-h-screen bg-white overflow-y-auto fixed inset-0 md:w-1/3 md:block md:static
@@ -23,40 +28,46 @@ function RecentOrders({ menuOn, setMenuOn, orders }: RecentOrdersMenu) {
           <CloseIcon />
         </button>
       </header>
-      {orders.map((order) => (
-        <article
-          className="p-2 border-b flex justify-between gap-4 m-2 bg-white"
-          key={order.id}
-        >
-          <img
-            src={order.src}
-            alt={order.name}
-            className="min-w-24 h-24 object-cover"
-          />
-          <div className="w-full">
-            <p className="font-bold">{order.name}</p>
-            <p className="text-gray-500">{order.type}</p>
-            <p className="text-orange-500 font-bold">${order.cost}</p>
-            <div className="flex justify-between items-center w-16">
-              <button className="bg-gray-200 w-5 h-5 rounded">-</button>
-              <p className="text-sm font-semibold text-gray-500">
-                {order.quantity}
-              </p>
-              <button className="bg-gray-200 w-5 h-5 rounded">+</button>
+      {orders.length > 0 ? (
+        orders.map((order) => (
+          <article
+            className="p-2 border-b flex justify-between gap-4 m-2 bg-white"
+            key={order.id}
+          >
+            <img
+              src={order.src}
+              alt={order.name}
+              className="min-w-24 h-24 object-cover"
+            />
+            <div className="w-full">
+              <p className="font-bold">{order.name}</p>
+              <p className="text-gray-500">{order.type}</p>
+              <p className="text-orange-500 font-bold">${order.cost}</p>
+              <div className="flex justify-between items-center w-16">
+                <button className="bg-gray-200 w-5 h-5 rounded">-</button>
+                <p className="text-sm font-semibold text-gray-500">
+                  {order.quantity}
+                </p>
+                <button className="bg-gray-200 w-5 h-5 rounded">+</button>
+              </div>
             </div>
-          </div>
-        </article>
-      ))}
+          </article>
+        ))
+      ) : (
+        <p className="p-4 font-semibold h-96 text-center flex justify-center items-center">
+          No hay ordenes recientes
+        </p>
+      )}
       <footer className="p-2 bg-white flex flex-col gap-4 sticky bottom-0">
         <div className="flex justify-between">
-          <p className="font-bold">Items(7)</p>
-          <p className="font-bold">$50.00</p>
+          <p className="font-bold">Items({totalItems})</p>
+          <p className="font-bold">${totalCost}</p>
         </div>
         <div className="flex justify-between border-b pb-2">
           <p>Propina 10%</p>
           <p className="font-bold">$5.00</p>
         </div>
-        <p>Total: $55.00</p>
+        <p>Total: ${totalCost}</p>
         <button
           className="w-full hover:bg-orange-100 h-10 rounded-xl bg-orange-500
          text-white font-bold"
